48df3a10e4abc21db9c58ff465170c688873245f
김명식 <112395484+D0ryeon@users.noreply.github.com>
2023-08-29 09:58:43 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.17 ms, Memory: 30.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.cs
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.cs
@@ -1,17 +1,16 @@
 public class Solution {
-    public int solution(long num) {
+    public int solution(int num) {
         int answer = 0;
-        while(num!=1){
-            
-            
+        while(!(num==1)){
+            if(answer==500){
+                return -1;
+            }
             if(num%2==0){
                 num=num/2;
+                answer++;
             }else{
                 num=num*3+1;
-            }
-            answer++;
-            if(answer>500){
-                return -1;
+                answer++;
             }
         }
         return answer;
